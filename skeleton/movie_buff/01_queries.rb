7312b6325ee3c15ebdad45171c0df6033da59493
jackcusick95 <jackcusick95@gmail.com>
2021-03-26 15:24:16 -0400

    Finish 01 problems, create first problem of 02

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -64,8 +64,8 @@ def directed_by_one_of(them)
 
   Movie
     .select(:id, :title)
-    .joins(:actors)
-    .where(actors: them)
+    .joins(:director)
+    .where(actors: { name: them } )
 
 end
 
@@ -80,5 +80,5 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where('yr < 1940').pluck(:title)
 end
